723df8e1cba82a05d9760d2af537f77e23c8f437
uchihasasukeeeeeee <almonteivanjake@gmail.com>
2024-10-17 16:09:56 +0800

    Create program1.cpp

---
+++ images/program1.cpp
@@ -0,0 +1,10 @@
+#include <iosteam>
+using namespace std;
+
+int main () {
+    string name;
+    cout<<"Welcome to C++ Language Programming;
+    cout<<"\nGood morning pal!, What is your name?";
+    cin>>name;
+    cout<<"Hi" + name +", Welcome to C++ Programming Class . \n
+}
